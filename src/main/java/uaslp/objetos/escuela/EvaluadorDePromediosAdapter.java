c3e668fd69127e46ad7c34b7c488a241e7b884e2
Tadeo <tadeolozanog2@gmail.com>
2022-05-25 19:28:49 -0500

    Segundo unit test pasado, el primero no me sale y tengo duda con el segundo

---
+++ src/main/java/uaslp/objetos/escuela/EvaluadorDePromediosAdapter.java
@@ -0,0 +1,23 @@
+package uaslp.objetos.escuela;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EvaluadorDePromediosAdapter {
+    private EvaluadorDePromedios promedios;
+
+    public EvaluadorDePromediosAdapter(EvaluadorDePromedios promedios) {
+        this.promedios = promedios;
+    }
+
+    public double evalua(String calis){
+        List<String> calificacionesString = Arrays.asList(calis.split(","));
+        List<Double> calificaciones = new ArrayList<>();
+        for (String calificacion : calificacionesString)
+        {
+            calificaciones.add(Double.parseDouble(calificacion));
+        }
+        return promedios.evalua(calificaciones);
+    }
+}
